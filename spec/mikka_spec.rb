be81c1d46004cc935a448f159b497d2bd62e8ced
Theo <theo@iconara.net>
2012-08-24 10:55:05 +0200

    A test that was lying around

--- spec/mikka_spec.rb
+++ spec/mikka_spec.rb
@@ -40,10 +40,31 @@ module Mikka
       actor = @system.actor_of(actor_props, 'some_actor')
       actor.should be_a(ActorRef)
     end
+  end
 
-    # future = actor.ask(:hi, 1000)
-    # reply = Mikka.await_result(future, :timeout => '1000ms')
-    # reply.should == :hi
-    
+  describe 'message sending' do
+    before do
+      @system = Mikka.create_actor_system('testsystem')
+      @actor = @system.actor_of(Props[TestActor])
+    end
+
+    after do
+      @system.shutdown
+    end
+
+    describe '#tell/#<<' do
+      it 'sends a message to an actor' do
+        @actor << 'hello'
+      end
+    end
+
+    describe '#ask' do
+      it 'sends a message'
+            # future = actor.ask(:hi, 1000)
+      # reply = Mikka.await_result(future, :timeout => '1000ms')
+      # reply.should == :hi
+
+      end
+    end
   end
 end
